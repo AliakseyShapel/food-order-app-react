b175450ae27542dfbb101583746fa8e7f4841684
Aliaksey Shapel <Aliaksey.Shapel@gmail.com>
2023-04-06 06:47:25 +0300

    136. Adding a Form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MealItemForm from './MealItemForm';
+
 import classes from './MealItem.module.css';
 
 export default function MealItem(props) {
@@ -11,7 +13,9 @@ export default function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 }
