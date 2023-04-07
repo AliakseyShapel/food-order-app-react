af51b458f63272741ff7e836ce7df37bd5c88654
Aliaksey Shapel <Aliaksey.Shapel@gmail.com>
2023-04-07 04:53:11 +0300

    141. Adding a Cart Context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CratProvider from './store/CartProvider';
 
 export default function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,12 +17,12 @@ export default function App() {
   }
 
   return (
-    <>
+    <CratProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CratProvider>
   );
 }
---
+++ src/store/CartProvider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import CartContext from './cart-context';
+
+export default function CratProvider(props) {
+  function addItemToCartHandler(item) {}
+
+  function removeItemFromCartHandler(id) {}
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
