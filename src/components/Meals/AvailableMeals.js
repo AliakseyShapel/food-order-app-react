fc27fa1cec8f7a6fdffb1b08a864d17f2be54981
AliakseyShapel <53071397+AliakseyShapel@users.noreply.github.com>
2023-04-11 15:01:39 +0300

    217. Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,14 +8,17 @@ import classes from './AvailableMeals.module.css';
 export default function AvailableMeals() {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-course-3ee95-default-rtdb.firebaseio.com/meals.json'
       );
+      if (!response.ok) {
+        throw new Error('Something went wrong...');
+      }
       const responseData = await response.json();
-
       const loadedMeals = [];
       for (const key in responseData) {
         loadedMeals.push({
@@ -28,13 +31,24 @@ export default function AvailableMeals() {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+    fetchMeals().catch((err) => {
+      setIsLoading(false);
+      setHttpError(err.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
